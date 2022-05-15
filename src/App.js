e8065b24fb8687a5992b6c645acb8bb9f7b13cb2
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2022-05-15 20:43:00 +0300

    Solve without challenge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/trips-list" element={<TripsList />} />
+        <Route path="/details/:tripSlug" element={<TripDetail />} />
+      </Routes>
     </div>
   );
 }
